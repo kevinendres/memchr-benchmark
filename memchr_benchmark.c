0e09dacc863701473c53707131002f3fd16cb6f5
kevin endres <k.m.endres@gmail.com>
2020-06-28 15:58:15 -0500

    changed buffer size to be a command line argument

--- memchr_benchmark.c
+++ memchr_benchmark.c
@@ -1,4 +1,5 @@
 //Returns time of memchr operation in nanoseconds
+//Takes args in order: 1) buffer_size
 
 #include <stdio.h>
 #include <string.h>
@@ -12,25 +13,27 @@ char random_char() {
     return c;
 }
 
-int main () {
+int main (int argc, char **argv) {
     //seed randomization and inits/decs
     srand(time(NULL));
-    char* mem_block = (char*) malloc(1000);
+    int buffer_size = argv[1];
+    char* mem_block = (char*) malloc(buffer_size);
     char character;
     char search_char = random_char(); 
     struct timespec start, end;
     long elapsed_time;
 
+
     //fill memory with random characters
-    for(int i = 0; i < 1000; i++) {
+    for(int i = 0; i < buffer_size; i++) {
         character = random_char();
         *( mem_block + i ) = character;
     }
 
     //measure
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
-    memchr(mem_block, search_char, 1000);
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    memchr(mem_block, search_char, buffer_size);
+    clock_gettime(CLOCK_MONOTONIC, &end);
     elapsed_time = (end.tv_sec * NANOSEC_CONVERSION + end.tv_nsec) - (start.tv_sec * NANOSEC_CONVERSION + start.tv_nsec);
 
     free(mem_block);
