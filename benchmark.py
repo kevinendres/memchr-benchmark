9e819b91a7f9bd0d46d1b32abeae20173c842ca7
kevin <k.m.endres@gmail.com>
2020-07-25 15:43:59 -0500

    added new bins to benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -9,13 +9,13 @@ sse_exe = "memchr_sse"
 glibc_exe = "memchr_glibc"
 simple_exe = "memchr_simple"
 executables = [avx2_exe, sse_exe, glibc_exe, simple_exe]
-optimizations = list("_O", "_O2", "_O3")
-for exe in (glibc_exe, simple_exe):
+optimizations = ["_O", "_O2", "_O3"]
+for exe in [glibc_exe, simple_exe]:
     for i in optimizations:
         executables.append(exe+i)
 
 # Experiment loop variables
-repetitions = 3
+repetitions = 2
 execution_times = list()
 
 # Experiment loop
@@ -33,20 +33,20 @@ for exe in executables:
     execution_times.append(times)
 
 # output processing
-for i, times in enumerate(execution_times):
-    min_time = min(times)
-    max_time = max(times)
-    mean_time = statistics.mean(times)
-    median_time = statistics.median(times)
-    std_dev = statistics.stdev(times) 
-    time_stamp = datetime.datetime.now().replace(microsecond=0).isoformat()
-    exe = executables[i]
-    
 with open('results.csv', 'a', newline='') as csv_file:
-    fields = ["Executable", "Min", "Max", "Mean", "Median", "Std. Deviation"]
+    fields = ["Executable", "Min", "Max", "Mean", "Median", "Std. Deviation", "Time Stamp"]
     csv_writer = csv.DictWriter(csv_file, fieldnames=fields)
-    csv_writer.writerow({ "Executable" : exe, "min" : min_time, "max" : max_time, "mean" : mean_time,\
-            "median" : median_time, "standard deviation" : std_dev, "time stamp" : time_stamp })
+    csv_writer.writeheader()
+    for i, times in enumerate(execution_times):
+        min_time = min(times)
+        max_time = max(times)
+        mean_time = statistics.mean(times)
+        median_time = statistics.median(times)
+        std_dev = statistics.stdev(times) 
+        time_stamp = datetime.datetime.now().replace(microsecond=0).isoformat()
+        exe = executables[i]
+        csv_writer.writerow({ "Executable" : exe, "Min" : min_time, "Max" : max_time, "Mean" : mean_time,\
+                "Median" : median_time, "Std. Deviation" : std_dev, "Time Stamp" : time_stamp })
 
 
 # data processing
