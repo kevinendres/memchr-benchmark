b2292ee95d9e5281d67cdae7872e89887634af62
Kevin Endres <k.m.endres@gmail.com>
2020-08-28 20:28:54 -0500

    added threading with synchronization after L3 cache sized blocks

--- main_pthread_blocks.c
+++ main_pthread_blocks.c
@@ -23,7 +23,7 @@
 
 #define NANOSEC_CONVERSION 1E9
 #define NUM_THREADS 10
-#define PAGE_SIZE sysconf(_SC_PAGESIZE) 
+#define CACHELINE_SIZE sysconf(_SC_LEVEL1_DCACHE_LINESIZE) 
 #define L3_SIZE sysconf(_SC_LEVEL3_CACHE_SIZE)
 
 /* GLOBAL */
@@ -33,40 +33,55 @@ char search_char;
 int chunk_size;
 int final_thread;
 int buffer_size;
-pthread_t tid[NUM_THREADS];
+struct return_val_storage {
+    char* thread_result;
+    char padding[CACHELINE_SIZE - sizeof(char*)];
+} return_vals[NUM_THREADS];
 
 /* Prototype */
 void *multi_memchr(void *vargp);
 
-int main (int argc, char **argv) {
+int main(int argc, char **argv) {
     //inits/decs
     buffer_size = BUFFER_SIZE;
     final_thread = NUM_THREADS - 1;
     mem_block = (char*) aligned_alloc(64, buffer_size);
+    char *mem_clear = mem_block;
     char fill_character = MEM_FILLER;
     search_char = SEARCH_STR; 
     struct timespec start, end;
     long elapsed_time;
+    return_val = NULL;
+    pthread_t tid[NUM_THREADS];
+    
 
     //thread related inits
     long myid[NUM_THREADS];
-    chunk_size = buffer_size / NUM_THREADS;    //each thread does chunk_size work before syncing, except final thread
+    chunk_size = L3_SIZE / NUM_THREADS;    //each thread does chunk_size work before syncing, except final thread
     //char *results[NUM_THREADS];
 
     //fill memory, set last byte to search_char
-    for(int i = 0; i < buffer_size; i++) {
-        *( mem_block + i ) = fill_character;
+    for (int i = 0; i < buffer_size; i++) {
+        *(mem_block + i) = fill_character;
     }
-    *( mem_block + chunk_size + 3) = search_char;
+    *(mem_block + L3_SIZE + 7) = search_char;
 
     //threading
     clock_gettime(CLOCK_MONOTONIC, &start);
-    for (int i = 0; i < NUM_THREADS; i++) {
-        myid[i] = i;
-        pthread_create(&tid[i], NULL, multi_memchr, &myid[i]);
-    }
-    for (int i = 0; i < NUM_THREADS; i++) {
-        pthread_join(tid[i], NULL);
+    while (buffer_size > 0) {
+        for (int i = 0; i < NUM_THREADS; i++) {
+            myid[i] = i;
+            pthread_create(&tid[i], NULL, multi_memchr, &myid[i]);
+        }
+        for (int i = 0; i < NUM_THREADS; i++) {
+            pthread_join(tid[i], NULL);
+        }
+        if (return_val != NULL) 
+            break;
+        buffer_size -= L3_SIZE;
+        mem_block += L3_SIZE;
+        if (buffer_size < L3_SIZE) 
+            chunk_size = buffer_size / NUM_THREADS;
     }
     clock_gettime(CLOCK_MONOTONIC, &end);
 
@@ -74,39 +89,23 @@ int main (int argc, char **argv) {
     printf("total elapsed time: %ld\n", elapsed_time);
     printf("number of threads: %d\n", NUM_THREADS);
 
-    free(mem_block);
+    free(mem_clear);
     exit(0);
 }
 
 void *multi_memchr(void *vargp)
 {
-    struct timespec local_start, local_end;
-    long local_elapsed_time;
     long myid = *((long *) vargp);
     int local_chunk_size;
     char *local_return_val;
     char *local_mem_block = mem_block + myid * chunk_size;
-    pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
     if (myid == final_thread) {
-        local_chunk_size = buffer_size - myid * chunk_size;
+        local_chunk_size = (buffer_size < L3_SIZE ? buffer_size : L3_SIZE) - myid * chunk_size;
     } else { 
         local_chunk_size = chunk_size; 
         }
-    clock_gettime(CLOCK_MONOTONIC, &local_start);
     local_return_val = memchr(local_mem_block, search_char, local_chunk_size);
-    if (local_return_val != NULL) {
-        printf("I'm the lucky thread: %d\n", myid);
+    if (local_return_val != NULL)
         return_val = local_return_val;
-        for (int i = 0; i < NUM_THREADS; i++) {
-            if (i != myid) {
-                printf("cancelling %d", i);
-                pthread_cancel(tid[i]);
-            }
-        }
-    }
-    clock_gettime(CLOCK_MONOTONIC, &local_end);
-    local_elapsed_time = (local_end.tv_sec * NANOSEC_CONVERSION + local_end.tv_nsec) - (local_start.tv_sec * NANOSEC_CONVERSION + local_start.tv_nsec);
-    printf("Thread %d finished in %ld with return val %x. Search start: %x search end: %x\n", myid, local_elapsed_time, local_return_val, \
-        local_mem_block, local_mem_block+local_chunk_size);
     return NULL;
 }
