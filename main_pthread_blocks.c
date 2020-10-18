ccfd20d57d913559a758a830216a9f0373a154f0
kevin endres <k.m.endres@gmail.com>
2020-10-18 16:43:32 -0500

    added argument parsing to main_pthread_blocks

--- main_pthread_blocks.c
+++ main_pthread_blocks.c
@@ -1,5 +1,4 @@
 //Spawns multiple threads, each calling memchr on different block of memory
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -10,9 +9,9 @@
 
 #include "memchr.h"
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 1000000007 
-#endif
+// #ifndef BUFFER_SIZE
+// # define BUFFER_SIZE 1000000007 
+// #endif
 
 #ifndef MEM_FILLER
 # define MEM_FILLER 0x42
@@ -23,46 +22,52 @@
 #endif
 
 #define NANOSEC_CONVERSION 1E9
-#define NUM_THREADS 20
-#define L3_SIZE sysconf(_SC_LEVEL3_CACHE_SIZE)
+// #define NUM_THREADS 20
+// #define L3_SIZE sysconf(_SC_LEVEL3_CACHE_SIZE)
 
-#ifndef BLOCK_SIZE
-# define BLOCK_SIZE L3_SIZE
-#endif
+// #ifndef BLOCK_SIZE
+// # define BLOCK_SIZE L3_SIZE
+// #endif
 
 /* GLOBALS */
 char *mem_block;
 char search_char;
 int block_size;
 int slice_size;
+int num_threads;
 int final_thread;       /* for handling remainders of uneven block sizes*/
 int buffer_size;
-char *return_vals[NUM_THREADS];
+char *return_vals;
 pthread_barrier_t sync_point;
 
 /* Prototype */
 void *multi_memchr(void *vargp);
 
 int main(int argc, char **argv) {
-    /*  */
-    int opt
-    
+    /* argument parsing */
+    int opt;
+    while((opt = getopt(argc, argv, "t:d:b:")) != -1) {
+        switch (opt) {
+            case 't': num_threads = atoi(optarg); break;
+            case 'd': buffer_size = atoi(optarg); break;
+            case 'b': block_size = atoi(optarg); break;
+        }
+    }
     /* inits/declarations */
-    buffer_size = BUFFER_SIZE;
-    final_thread = NUM_THREADS - 1;
+    return_vals = malloc(num_threads);
+    final_thread = num_threads - 1;
     mem_block = (char*) aligned_alloc(64, buffer_size);
     char fill_character = MEM_FILLER;
     search_char = SEARCH_STR; 
     struct timespec start, end;
     long elapsed_time;
     char *return_val = NULL;
-    pthread_t tid[NUM_THREADS];
+    pthread_t tid[num_threads];
 
     /* thread related inits */
-    long my_id[NUM_THREADS];
-    block_size = BLOCK_SIZE;
-    slice_size = block_size / NUM_THREADS;    /* each thread does slice_size work before syncing, except final thread */
-    pthread_barrier_init(&sync_point, NULL, NUM_THREADS);
+    long my_id[num_threads];
+    slice_size = block_size / num_threads;    /* each thread does slice_size work before syncing, except final thread */
+    pthread_barrier_init(&sync_point, NULL, num_threads);
 
     /* fill memory, set last byte to search_char */
     for (int i = 0; i < buffer_size; i++) {
@@ -72,14 +77,14 @@ int main(int argc, char **argv) {
 
     /* spawn and join threads with timer */
     clock_gettime(CLOCK_MONOTONIC, &start);
-    for (int i = 0; i < NUM_THREADS; i++) {
+    for (int i = 0; i < num_threads; i++) {
         my_id[i] = i;
         pthread_create(&tid[i], NULL, multi_memchr, &my_id[i]);
     }
-    for (int i = 0; i < NUM_THREADS; i++) {
+    for (int i = 0; i < num_threads; i++) {
         pthread_join(tid[i], NULL);
     }
-    for (int i = 0; i < NUM_THREADS; i++) {
+    for (int i = 0; i < num_threads; i++) {
         if (return_vals[i] != NULL) {
             return_val = return_vals[i];
             break;
@@ -92,6 +97,7 @@ int main(int argc, char **argv) {
     printf("%ld", elapsed_time);
 
     free(mem_block);
+    free(return_vals)
     exit(0);
 }
 
@@ -117,7 +123,7 @@ void *multi_memchr(void *vargp)
         //sync and check for hits
         pthread_barrier_wait(&sync_point);
         return_vals[my_id] = local_return_val;
-        for (int j = 0; j < NUM_THREADS; j++) {
+        for (int j = 0; j < num_threads; j++) {
             if (return_vals[j] != NULL) {
                 return NULL;
             }
@@ -129,7 +135,7 @@ void *multi_memchr(void *vargp)
         /* update sizes if last block of memory is smaller than the block_size */
         if (remaining_bytes < block_size) {
             local_block_size = remaining_bytes;
-            local_slice_size = local_block_size / NUM_THREADS;
+            local_slice_size = local_block_size / num_threads;
         }
     }
     return NULL;
